22133c87e1d892f1eec4a84e695c6d64743e5f1b
chamuditha123 <chamukeku@gmail.com>
2024-06-12 23:31:24 +0530

    setup keycloak

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
+import Keycloak from 'keycloak-js';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+let initOptions = {
+  url: 'http://localhost:8080/',
+  realm: 'myrealm',
+  clientId: 'react-client',
+}
+
+let kc = new Keycloak(initOptions);
+
+kc.init({
+  onLoad: 'login-required', // Supported values: 'check-sso' , 'login-required'
+  checkLoginIframe: true,
+  pkceMethod: 'S256'
+}).then((auth) => {
+  if (!auth) {
+    window.location.reload();
+  } else {
+    /* Remove below logs if you are using this on production */
+    console.info("Authenticated");
+    console.log('auth', auth)
+    console.log('Keycloak', kc)
+    console.log('Access Token', kc.token)
+
+    kc.onTokenExpired = () => {
+      console.log('token expired')
+    }
+  }
+}, () => {
+  /* Notify the user if necessary */
+  console.error("Authentication Failed");
+});
 
 function App() {
+
+  const [infoMessage, setInfoMessage] = useState('');
+
   return (
-    <div>
-      Hi
-    </div>
+    <>
+      <div>
+        <h1>React App</h1>
+      </div>
+      
+      <div>
+        <Button onClick={() => {setInfoMessage(
+          kc.authenticated ? 'Authenticated: TRUE' : 'Authenticated: FALSE'
+        ); }}/>
+        <Button onClick={() => { kc.login() }}/>
+        <Button onClick={() => { kc.logout({
+          redirectUri: 'http://localhost:3000/'
+        }) }}/>
+      </div>
+
+      <div>
+        <Card>
+          <p style={{ wordBreak: 'break-all' }} id='infoPanel'>
+            {infoMessage}
+          </p>
+        </Card>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
